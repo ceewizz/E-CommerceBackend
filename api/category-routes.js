d6391e114d66cc6ef139ab3cdd8723a277adf59b
ceewizz <ceewizzhuhwin@gmail.com>
2024-01-11 23:22:39 -0800

    finished with category-routes.js with index.js in the api

--- api/category-routes.js
+++ api/category-routes.js
@@ -39,12 +39,85 @@ router.get('/', async (req, res) => {
         }
 
         Products.create(req.body)
+        .then((products) => {
+            // Create pairings for ProductsTag models
+            if (req.body.tagsIds.length) {
+                const productsTagIdArr = req.body.tagsIds.map((tags_id) => {
+                    return {
+                        products_id: products.id,
+                        tags_id,
 
-        }
-    })
-
-        }
+                    };
+                });
+                return ProductsTag.bulkCreate(productsTagIdArr);
             }
+            res.status(200).json(product);
         })
+        .then((productsTagIds) => res.status(200).json(productsTagIds))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
 
-    )
+        });
+                });
+
+                // updating the products
+                router.put('/:id', (req, res) => { 
+
+                    // data updating
+                    Products.update(req.body, {
+                        where: {
+                            id: req.params.id,
+
+
+                        },
+
+                    })
+                    .then((products) => {
+                        // Finding tags associated from the ProductsTag
+                        return ProductsTag.findAll({ where:   { products_id: req.params.id} });
+                        })
+                        .then((product) => {
+                            const productsTagIds = productsTag.map(({ tags_id }) => tags_id);
+
+                            const newProductTags = req.body.tagsIds
+                            .filter((tags_id) => !productsTagIds.includes(tags_id))
+                            .map((tags_id) => {
+                                return {
+                                    products_id: req.params.id,
+                                    tags_id,
+                                };
+                            });
+
+                            const productTagsToRemove = productTags
+                            .filter(({ tags_id} => !req.body.tagsIds.includes(tags_id))
+                            .map(({ id }) => id);
+
+                            return Promise.all([
+                                ProductsTag.destroy({ where: { id: productTagsToremove} }),
+                                ProductsTag.bulkCreate(newProductTags),
+                            ]);
+                        })
+                        .then((updateProductTags) => res.json(updatedProductTags))
+                        .catch((err) =>{
+                            res.json(err);
+
+                        });
+                    });
+                    router.delete(':/id', async (req, res) => {
+                        try {
+                            const productsData = await Products.destroy({ where: { id: req.params.id } });
+                        if(!productsData) {
+                            res.status(404).json({ message: 'Product not found' });
+                            return;
+                        }
+                        res.status(200).json(productsData);
+                    } catch(err) {
+                        res.status(500).json(err)
+                    }
+                });
+
+                module.exports = router;
+                
+
+
